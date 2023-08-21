cd47f0f87bf985d0005676c6abb8d214813bd20f
carter-lilley <56591072+carter-lilley@users.noreply.github.com>
2023-08-20 21:25:44 -0400

    TAKING OUT THE GARBAGE

--- resources/scripts/classes/cb_player_class.gd
+++ resources/scripts/classes/cb_player_class.gd
@@ -6,9 +6,14 @@ var prev_vel: Vector3
 @export var dot_factor: Curve
 
 @export_category("Turning")
-@export var turn: bool = true
-@export var turn_DEBUG: bool = true
 @export var turn_speed: float = .1  # Adjust the desired turn speed as needed
 @export var turn_damp: float = 0.02  # Adjust the base proportional gain as needed
 
-@export var stopping_torque_factor: float = 45 #Higher values will result in stronger stopping torque
+@export var pID: int
+@onready var cam: Node = $"../Camera3D"
+@onready var model: Node = $guy_import
+@export var collision_impulse: float = 1.0
+
+@export var ACCEL: float = 10.0
+@export var MAX_SPEED: float = 5.0
+@export var JUMP_VELOCITY = 4.5
